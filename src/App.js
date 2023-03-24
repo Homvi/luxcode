ddce0307dea27fd44a17316d964f4482990d2e32
homvi <honvedoadamkj@gmail.com>
2023-03-24 18:43:32 +0100

    change subheading size

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import Landing from "./pages/Landing";
 
 function App() {
   return (
-    <div className="p-5 bg-gradient-to-br from-neutral-700 to-neutral-900 h-screen">
+    <div className="p-5 bg-gradient-to-br from-neutral-700 via-neutral-800 to-neutral-900 h-screen">
       <Landing />
     </div>
   );
--- src/components/Subheading.jsx
+++ src/components/Subheading.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Subheading = ({ content }) => {
   return (
-    <div className="text-sm mb-5 inter text-transparent bg-clip-text bg-gradient-to-br from-[#B8A080] to-[#DEA96B]">
+    <div className="text-xs mb-5 inter text-transparent bg-clip-text bg-gradient-to-br from-[#B8A080] to-[#DEA96B]">
       {content}
     </div>
   );
