92230d7e1e1c8ad0b017368927130867c4305565
homvi <honvedoadamkj@gmail.com>
2023-03-25 13:11:22 +0100

    add first heading

---
+++ src/components/Heading.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Heading = ({ content }) => {
+  return <h1 className="text-4xl font-semibold inter text-transparent bg-clip-text bg-gradient-to-br from-[#B8A080] to-[#DEA96B]" >{content}</h1>;
+};
+
+export default Heading;
--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -1,15 +1,22 @@
-import React from 'react'
-import Navbar from '../components/Navbar'
-import Subheading from '../components/Subheading'
+import React from "react";
+import Heading from "../components/Heading";
+import Navbar from "../components/Navbar";
+import Subheading from "../components/Subheading";
 
 const Landing = () => {
   return (
     <div className="">
-        <Navbar/>
-        <Subheading content="Minőségi weboldalak, elegáns megjelenés - LuxCode"  />
-        <Subheading content="Elégedett ügyfelek mondták rólunk"  />
+      <Navbar />
+      <div className="mt-12">
+        <Subheading content="Minőségi weboldalak, elegáns megjelenés - LuxCode" />
+      </div>
+      <div className="mb-5">
+        <Heading content="Egyedi tervezésű, " />
+        <Heading content="gyönyörű weboldalak " />
+      </div>
+      <Subheading content="Elégedett ügyfelek mondták rólunk" />
     </div>
-  )
-}
+  );
+};
 
-export default Landing
+export default Landing;
