c3223a40885e37b7b5150a7a5c8e4b1c49447169
homvi <honvedoadamkj@gmail.com>
2023-03-25 13:21:42 +0100

    add desc

---
+++ src/components/Description.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Description = ({ content }) => {
+  return <div className="text-[#757575] max-w-sm leading-5 text-xs inter mb-5">{content}</div>;
+};
+
+export default Description;
--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -1,7 +1,13 @@
 import React from "react";
+import Description from "../components/Description";
 import Heading from "../components/Heading";
 import Navbar from "../components/Navbar";
 import Subheading from "../components/Subheading";
+
+const data = {
+  description:
+    "Üdvözöljük a LuxCode-nál! Egyedi és elegáns weboldalak tervezése és fejlesztése az Ön vállalkozása számára. Profi csapatunk gondoskodik a tökéletes online megjelenésről. Legyen sikeres velünk az internetes világban!",
+};
 
 const Landing = () => {
   return (
@@ -14,6 +20,7 @@ const Landing = () => {
         <Heading content="Egyedi tervezésű, " />
         <Heading content="gyönyörű weboldalak " />
       </div>
+      <Description content={data.description} />
       <Subheading content="Elégedett ügyfelek mondták rólunk" />
     </div>
   );
