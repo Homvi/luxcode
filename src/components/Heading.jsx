64b602f4dafc734f40baf11008e4a3c1e93e05b4
homvi <honvedoadamkj@gmail.com>
2023-03-25 14:56:05 +0100

    change style of main heading

--- src/components/Heading.jsx
+++ src/components/Heading.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const Heading = ({ content }) => {
-  return <h1 className="text-4xl font-semibold inter text-transparent bg-clip-text bg-gradient-to-br from-orange-200 to-orange-400" >{content}</h1>;
+  return <h1 className="text-4xl font-semibold inter text-transparent bg-clip-text bg-gradient-to-br from-orange-100 via-orange-300 to-orange-300" >{content}</h1>;
 };
 
 export default Heading;
