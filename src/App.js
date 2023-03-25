b837af1174b6a308071ea743adedf0ca1d9f991d
homvi <honvedoadamkj@gmail.com>
2023-03-25 15:04:55 +0100

    change favicon

--- src/App.js
+++ src/App.js
@@ -6,14 +6,7 @@ function App() {
     <>
       <Helmet>
         <meta charSet="utf-8" />
-        <title>Luxcode</title>
-        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png"/>
-        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png"/>
-        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png"/>
-        <link rel="manifest" href="/site.webmanifest"/>
-        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5"/>
-        <meta name="msapplication-TileColor" content="#3d3d3d"/>
-        <meta name="theme-color" content="#000000"/>
+       
       </Helmet>
       <div className="p-5 bg-gradient-to-br from-neutral-800 via-neutral-800 to-neutral-900 min-h-screen">
         <Landing />
