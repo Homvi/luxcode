bb7efcba9eff021d380c4a774e06d38199fbe0c5
homvi <honvedoadamkj@gmail.com>
2023-04-07 10:54:35 +0200

    change bcg to svg

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import pc from "./assets/pc.png"
 function App() {
   return (
     <>
-      <div className="p-8 bg-gradient-to-br from-neutral-800 via-neutral-800 to-neutral-900 min-h-screen relative">
+      <div className="p-8 bg-cover  svg-bcg min-h-screen relative">
         <img src={pc}  alt="pc" className="absolute right-0 bottom-0 h-[90%] hidden lg:block"/>
         <Landing />
       </div>
