aea9e6ba6cb3a9b4386650811f26f0015b471700
homvi <honvedoadamkj@gmail.com>
2023-04-09 12:49:53 +0200

    change route name

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,7 @@ function App() {
           <Route path="/" element={<Landing />} />
           <Route path="/services" element={<Services />} />
           <Route path="/quote" element={<Quote />} />
-          <Route path="/success" element={<Success />} />
+          <Route path="/successpage" element={<Success />} />
         </Routes>
       </div>
     </>
