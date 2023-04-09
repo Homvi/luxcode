6cc086615cc16aa7b82c26929a75ee00b2b7b7d6
homvi <honvedoadamkj@gmail.com>
2023-04-09 12:55:47 +0200

    change language and for action root

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Form = () => {
   return (
-    <form className="w-full" name="quote" method="post" action="/successpage/">
+    <form className="w-full" name="quote" method="post" action="/successpage">
         
         <input type="hidden" name="form-name" value="quote" />
       {/* név */}
