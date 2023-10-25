94373ff6e0b8f6a714eb1e2d77857196de1fb18e
Homvi <honvedoadamkj@gmail.com>
2023-10-25 12:24:54 +0200

    add with to logo

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -15,7 +15,7 @@ const Navbar = () => {
     <>
       <div className="flex justify-between  relative">
         <Link to="/">
-          <img className="h-8 mb-5" src={luxCodeLogo} alt="LuxCode" />
+          <img className="h-8 w-auto mb-5" src={luxCodeLogo} alt="LuxCode" />
         </Link>
         <div className="flex flex-col justify-center items-center">
           <div
